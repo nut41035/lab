e57ad4dedbf25e410721b0f305cd72f8d337a464
natsu <nut41035@gmail.com>
2019-11-15 21:51:39 +0900

    add whole body predict

--- predict.py
+++ predict.py
@@ -4,13 +4,14 @@ from tensorflow import keras
 import os
 import cv2 as cv
 import glob
+import math
 from imageio import imwrite
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 
 from data import *
 
-def test(BASE_DIR = './data/processed/only_cancers/',MODEL_VERSION = 'UNet_20191115-162049'):
+def test(BASE_DIR, MODEL_VERSION):
     # 0 = all messages are logged (default behavior)
     # 1 = INFO messages are not printed
     # 2 = INFO and WARNING messages are not printed
@@ -61,3 +62,57 @@ def test(BASE_DIR = './data/processed/only_cancers/',MODEL_VERSION = 'UNet_20191
         a.set_title('Mask')
 
         plt.savefig('%sresult/combined/%s.png'%(BASE_DIR, imgName))
+
+def whole_body_test(image_path, image_name, MODEL_VERSION, h = 64, w = 64):
+    # 0 = all messages are logged (default behavior)
+    # 1 = INFO messages are not printed
+    # 2 = INFO and WARNING messages are not printed
+    # 3 = INFO, WARNING, and ERROR messages are not printed
+    os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2' 
+    print("#### Start program")
+
+    print("Num GPUs Available: ", len(tf.config.experimental.list_physical_devices('GPU')))
+    if not os.path.exists('./result'):
+        os.makedirs('./result')
+
+    model = keras.models.load_model('model/%s.h5'%MODEL_VERSION)
+    print('#### Model loaded')
+
+    print(image_name)
+    path = image_path + 'images/' + image_name + '.jpg'
+    image = cv.imread(path, 1)
+    mask_path = image_path + 'masks/' + image_name + '.jpg'
+    mask = cv.imread(mask_path, 1)
+
+    height = image.shape[0]
+    width = image.shape[1] 
+    hloop = int(math.floor(width/w)) #Horizontal loop
+    vloop = int(math.floor(height/h)) #Vertical loop
+    result = np.array([[0.0 for x in range(width)] for y in range(height)])
+    for i in range(hloop):
+        for j in range(vloop):
+            crop_img = image[h*j:h*(j+1), w*i:w*(i+1)]
+            crop_img = np.array([crop_img/255.0])
+            predictions = model.predict(crop_img)
+            predictions = np.squeeze(predictions)
+
+            result[h*j:h*(j+1), w*i:w*(i+1)] = predictions
+    imwrite('./result/%s_predicted.png'%image_name, result)
+              
+    fig = plt.figure()
+    a = fig.add_subplot(1, 3, 1)
+    imgplot = plt.imshow(image)
+    a.set_title('Original')
+
+    a = fig.add_subplot(1, 3, 2)
+    result = cv.imread('./result/%s_predicted.png'%image_name, 1)
+    imgplot = plt.imshow(result)
+    a.set_title('Predictions')
+
+    a = fig.add_subplot(1, 3, 3)
+    imgplot = plt.imshow(mask)
+    print(mask.shape)
+    a.set_title('Mask')
+
+   
+    plt.savefig('./result/%s_combined.png'%image_name)
