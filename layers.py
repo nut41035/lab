e15807084b18117e0f7ef0d64dde4c3f7d2c39ac
natsu <nut41035@gmail.com>
2019-11-17 21:43:26 +0900

    modify layer

--- layers.py
+++ layers.py
@@ -14,3 +14,22 @@ def up_sampling(Input1, Input2, target_size, kernel_size = 3, padding = "same", 
     conv = keras.layers.Conv2D(target_size, kernel_size, padding = padding, strides = stride, activation=activation)(merge)
     conv = keras.layers.Conv2D(target_size, kernel_size, padding = padding, strides = stride, activation=activation)(conv)
     return conv
+
+def down_sampling_with_norm(Input, target_size, kernel_size = 3, padding = "same", activation = "relu", stride = 1):
+    conv = keras.layers.Conv2D(target_size, kernel_size, padding = padding, strides = stride)(Input)
+    conv = keras.layers.BatchNormalization()(conv)
+    conv = keras.layers.Conv2D(target_size, kernel_size, padding = padding, strides = stride)(conv)
+    conv = keras.layers.BatchNormalization()(conv)
+    pool = keras.layers.MaxPool2D(pool_size=(2, 2))(conv)
+    return conv, pool
+
+def up_sampling_with_norm(Input1, Input2, target_size, kernel_size = 3, padding = "same", activation = "relu", stride = 1):
+    # Input1 is from previous layer
+    # Input2 is layer to concat
+    pool = keras.layers.UpSampling2D((2, 2))(Input1)
+    merge = keras.layers.Concatenate()([pool,Input2])
+    conv = keras.layers.Conv2D(target_size, kernel_size, padding = padding, strides = stride, activation=activation)(merge)
+    conv = keras.layers.BatchNormalization()(conv)
+    conv = keras.layers.Conv2D(target_size, kernel_size, padding = padding, strides = stride, activation=activation)(conv)
+    conv = keras.layers.BatchNormalization()(conv)
+    return conv
