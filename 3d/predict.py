86f18235777bcf906c56c72dfef5e83e4319fc08
natsu <nut41035@gmail.com>
2020-01-27 15:34:42 +0900

    predict with color

--- 3d/predict.py
+++ 3d/predict.py
@@ -23,7 +23,7 @@ config = ConfigProto()
 config.gpu_options.allow_growth = True
 session = InteractiveSession(config=config)
 
-def predict(MODEL, base_path, img_name, save=True):
+def predict(MODEL, base_path, img_name, save_to_file=True):
     image_path = os.path.join(base_path, 'images/', img_name)
     image = np.load(image_path)
     image = np.float32(image)
@@ -32,34 +32,63 @@ def predict(MODEL, base_path, img_name, save=True):
     prediction = MODEL.predict(image)
     prediction = np.squeeze(prediction)
     prediction = np.where(prediction < 0.7, 0, 255)
-    
-    if save:
+    TP = 0
+    FN = 0
+    FP = 0
+    if save_to_file:
         mask_path = os.path.join(base_path, 'masks/', img_name)
         mask = np.load(mask_path)
         index = int(img_name[2:6])
+        red = np.array([0,0,255])
+        green = np.array([0,255,0])
+        blue = np.array([255,0,0])
         for i in range(32):
-            fig = plt.figure()
-            a = fig.add_subplot(1, 3, 1)
-            imgplot = plt.imshow(image[0,:,:,i,:].astype(np.uint8))
-            a.set_title('Original')
+            img = image[0,:,:,i,:].astype(np.uint8)
+            save = False
+            for j in range(64):
+                for k in range(64):
+                    m_pixel = mask[j,k,i]
+                    p_pixel = prediction[j,k,i]
+                    if m_pixel == 1 or p_pixel == 255:
+                        save = True
+                    if m_pixel == 1 and p_pixel == 255: #TP
+                        img[j,k,:] = green
+                        TP+=1
+                    elif m_pixel == 1 and p_pixel == 0: #FN
+                        img[j,k,:] = red
+                        FN+=1
+                    elif m_pixel == 0 and p_pixel == 255: #FP
+                        img[j,k,:] = blue
+                        FP+=1
+            if True:
+                new_name = img_name[:-4] + "__%02d" %(index)
+                # fig = plt.figure()
+                # fig.suptitle(new_name, fontsize=20)
+                # a = fig.add_subplot(1, 2, 1)
+                # imgplot = plt.imshow(image[0,:,:,i,:].astype(np.uint8))
+                # a.set_title('Original')
 
-            a = fig.add_subplot(1, 3, 2)
-            imgplot = plt.imshow(prediction[:,:,i])
-            a.set_title('Predictions')
+                # a = fig.add_subplot(1, 2, 2)
+                # imgplot = plt.imshow(img)
+                # a.set_title('Predictions')
 
-            a = fig.add_subplot(1, 3, 3)
-            imgplot = plt.imshow(mask[:,:,i])
-            a.set_title('Mask')
-
-            new_name = img_name[:4] + "%02d" %(index) + img_name[6:]
-            plt.savefig('./result/%s.png'%new_name)
-            plt.close()
+                # plt.savefig('./result/%s.png'%new_name)
+                # plt.close()
+                cv2.imwrite('./result/%s.png'%new_name, img)
+                cv2.imwrite('./result/%s.png'%img_name[:-4], image[0,:,:,i,:])
             index += 1
 
-    return prediction
+    return TP,FP,FN
 
 def predict_folder(MODEL, base_path, save=True):
     files = glob.glob('%simages/*'%base_path)
+    TP_sum = 0
+    FP_sum = 0
+    FN_sum = 0
     for path in tqdm(files, desc='images in folder', leave=False):
         img_name = os.path.basename(path)
-        predict(MODEL, base_path, img_name, save)
+        TP, FN, FP = predict(MODEL, base_path, img_name, save)
+        TP_sum += TP
+        FN_sum += FN
+        FP_sum += FP
+    return TP_sum, FP_sum, FN_sum
